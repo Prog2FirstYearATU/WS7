603fad09d44588798d0a5afe02c7ad5b2eabfed0
Una <una.lestrange@atu.ie>
2026-03-26 10:46:22 +0000

    Slowly building Part2. The restock is done - need to add error checking for the quantity. The process sale transaction only handles one item at a time

--- Part2ProductSales/Program.cs
+++ Part2ProductSales/Program.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Part2ProductSales
 {
     internal class Program
     {
 
-       static List<int> myList = new List<int>();
 
-        static List<Product > products = new List<Product>();
+        static List<int> myList = new List<int>();
+
+        static List<Product> products = new List<Product>();
 
 
 
@@ -17,6 +19,7 @@ namespace Part2ProductSales
 
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
             products.Add(new Product("Eggs", 3.5m, 20));
             products.Add(new Product("Bread", 2.0m, 20));
             products.Add(new Product("Jam", 3.7m, 25));
@@ -49,10 +52,10 @@ namespace Part2ProductSales
             switch (choice)
             {
                 case 1:
-                   ProcessingSalesTransaction();
+                    ProcessingSalesTransaction();
                     break;
                 case 2:
-                    Console.WriteLine("Restocking Product...");
+                    RestockProduct();
                     break;
                 case 3:
                     Console.WriteLine("Printing Report...");
@@ -64,6 +67,14 @@ namespace Part2ProductSales
 
         }
 
+
+        /// <summary>
+        /// For now this only allows one sale.
+        /// The question asks for multiple sales to be allowed
+        /// This requires more data to be kept,
+        /// another class to hold data about a shopping cart
+        /// might be useful
+        /// </summary>
         static void ProcessingSalesTransaction()
         {
             Console.WriteLine("Processing Sales Transaction");
@@ -79,9 +90,53 @@ namespace Part2ProductSales
 
             int choice = productMenu.GetMenuChoice();
 
-            Console.WriteLine(choice);
+            Product chosenProduct = products[choice - 1];
+
+            Console.WriteLine($"How much {chosenProduct.Name} do you want?");
+
+            // This is not robust, but it is just a demo
+            // In a real application, you would want to add error handling here
+
+            int quantity = int.Parse(Console.ReadLine()!);
+
+            bool enoughProduct = chosenProduct.Sell(quantity);
+
+            if (!enoughProduct)
+            {
+                Console.WriteLine("Sorry not enough in stock");
+            }
+            else
+            {
+                Console.WriteLine($"You bought {quantity} {chosenProduct.Name} for {quantity * chosenProduct.UnitPrice:C}");
+            }
 
         }
 
+
+        static void RestockProduct()
+        {
+            Console.WriteLine("Restocking Product");
+            string[] productMenuItems = new string[products.Count];
+            for (int i = 0; i < products.Count; i++)
+            {
+                productMenuItems[i] = products[i].ToString();
+            }
+
+            Console.WriteLine("Which product do you want to restock?");
+
+            Menu productMenu = new Menu(productMenuItems);
+
+
+            /// this is not roubut, but it is just a demo
+            int choice = productMenu.GetMenuChoice();
+
+            Product chosenProduct = products[choice - 1];
+
+            Console.WriteLine("How much do you want to restock?");
+
+            int quantity = int.Parse(Console.ReadLine()!);
+
+            chosenProduct.Restock(quantity);
+        }
     }
 }
