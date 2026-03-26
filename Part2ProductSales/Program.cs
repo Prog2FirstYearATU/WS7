4c30ce3bdad81fcad6ce06eb542540175da19761
Una <una.lestrange@atu.ie>
2026-03-26 09:48:06 +0000

    displaying menu

--- Part2ProductSales/Program.cs
+++ Part2ProductSales/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] menuChoices =
+                 {"Process sale Transaction", "Restock Product", "Print report", "Quit"};
+
+            Menu menu = new Menu(menuChoices);
+
+
+
+            int choice = menu.GetMenuChoice();
+
+
+            while (choice != 4)
+            {
+
+                Console.WriteLine($"Choice was {choice}");
+                choice = menu.GetMenuChoice();
+            }
+            
+
         }
     }
 }
